0af92b8534d52d9519ecd0957fa652459e91cab7
seo <seo0h9@gmail.com>
2024-03-14 00:21:26 +0900

    [SP-2] 💄 Fix. Project page header 마진 추가

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,6 +1,6 @@
 import { useId } from 'react';
 
-import Project, { matter } from '@contents/project/sobisa.mdx';
+import Project, { matter } from '@contents/project/sobisa/write.mdx';
 
 import { components } from '@/components/mdx';
 import { SkillTag } from '@/components/skill-tag';
@@ -9,14 +9,15 @@ const ProjectPage = () => {
   const id = useId();
   return (
     <>
-      <div>{matter.duration}</div>
-      <h1 className='text-3xl font-bold'>{matter.title}</h1>
-      <div className='*:mr-2 *:text-sm'>
-        {matter.skillTag.map((skill) => (
-          <SkillTag key={`${id}-${skill}`} skill={skill} />
-        ))}
+      <div className='mb-8 flex flex-col gap-2'>
+        <div>{matter.duration}</div>
+        <h1 className='text-3xl font-bold'>{matter.title}</h1>
+        <div className='*:mr-2 *:text-sm'>
+          {matter.skillTag.map((skill) => (
+            <SkillTag key={`${id}-${skill}`} skill={skill} />
+          ))}
+        </div>
       </div>
-
       <Project components={components} />
     </>
   );
