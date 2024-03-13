970f60aecd404cd23efa0bccc404d1df0a687dcd
seo <seo0h9@gmail.com>
2024-03-14 00:19:53 +0900

    [SP-2] ✨ Add. Image modal 구현

---
+++ contents/project/components/img-gallery/img-moal.tsx
@@ -0,0 +1,28 @@
+import { ComponentProps } from 'react';
+
+import { Caption } from '@contents/project/components/img-gallery/main';
+
+import { Portal } from '@/components/common/portal';
+import { cn } from '@/utils/cn';
+
+export const ImageModal = ({
+  onClick,
+  className,
+  caption,
+  ...props
+}: ComponentProps<'img'> & { caption?: Caption }) => {
+  return (
+    <Portal>
+      <div className='fixed left-0 top-0 h-full w-full overflow-hidden' {...{ onClick }}>
+        <div className='absolute top-0 z-10 flex h-full w-full items-center justify-center '>
+          <div className='flex max-w-lg flex-col gap-2  rounded-xl border border-gray-400 bg-gray-800 bg-opacity-30 p-3 text-white backdrop-blur-sm'>
+            <img className={cn('rounded-md', className)} {...props} />
+            <h2 className='w-fit text-xl font-bold'>{caption?.heading}</h2>
+            <p className='break-words break-keep text-sm'>{caption?.descriptions}</p>
+          </div>
+        </div>
+        <div className='h-full bg-slate-800 bg-opacity-60 ' />
+      </div>
+    </Portal>
+  );
+};
