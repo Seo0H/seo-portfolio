764ee01f6b8d65d71506318cde954452cc193cc4
seo <seo0h9@gmail.com>
2024-03-14 01:46:08 +0900

    [SP-2] ✨ Add. Image modal description 에 mdx 문법을 사용할 수 있도록 mdx parser 추가

--- contents/project/components/img-gallery/img-moal.tsx
+++ contents/project/components/img-gallery/img-moal.tsx
@@ -3,6 +3,7 @@ import { ComponentProps } from 'react';
 import { Caption } from '@contents/project/components/img-gallery/main';
 
 import { Portal } from '@/components/common/portal';
+import { Markdown } from '@/components/mdx/utils/markdown-parer';
 import { cn } from '@/utils/cn';
 
 export const ImageModal = ({
@@ -18,7 +19,10 @@ export const ImageModal = ({
           <div className='flex max-w-lg flex-col gap-2  rounded-xl border border-gray-400 bg-gray-800 bg-opacity-30 p-3 text-white backdrop-blur-sm'>
             <img className={cn('rounded-md', className)} {...props} />
             <h2 className='w-fit text-xl font-bold'>{caption?.heading}</h2>
-            <p className='break-words break-keep text-sm'>{caption?.descriptions}</p>
+
+            <p className='break-words break-keep text-sm font-light [&_a]:underline'>
+              <Markdown>{caption?.descriptions}</Markdown>
+            </p>
           </div>
         </div>
         <div className='h-full bg-slate-800 bg-opacity-60 ' />
