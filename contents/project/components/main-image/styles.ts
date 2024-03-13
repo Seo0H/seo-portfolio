a569bcd580d642a2a8e52fde0c4bbc0a809d8b88
seo <seo0h9@gmail.com>
2024-03-14 00:18:54 +0900

    [SP-2] 🚚 Chore. mdx 폴더 경로를 ./contents 로 분리
     mdx와 관련된 컴포넌트, 내용을 분리하기 위함

---
+++ contents/project/components/main-image/styles.ts
@@ -0,0 +1,1 @@
+export const mainGridStyle = `grid-cols-[1.6fr_repeat(2,_1fr)]`;
---
+++ contents/project/components/main-image/types.ts
@@ -0,0 +1,3 @@
+import type { Caption } from '@contents/project/components/img-gallery/main';
+
+export type MainImageItemContents = { caption?: Caption; src: string; className?: string };
