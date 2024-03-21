691806a16c6452d3d4bf042b228adc79d13dc112
seo <seo0h9@gmail.com>
2024-03-21 22:03:01 +0900

    [SP-22] 💄 UI. Header 스타일 업데이트

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,30 +1,43 @@
 import { ComponentProps, useId } from 'react';
 
-import { Link } from 'react-router-dom';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 
+import { Logo } from '@/asset/logo/isSeo';
 import { projectKeys } from '@/pages/project';
+import { buttons } from '@/styles/buttons';
 import { cn } from '@/utils/cn';
+
+import './index.css';
 
 export const Header = ({ className }: Pick<ComponentProps<'div'>, 'className'>) => {
   const uniqId = useId();
+  const navigator = useNavigate();
 
   return (
-    <div
-      className={cn(
-        'fixed z-10 w-full bg-gray-100 bg-opacity-[0.8] bg-blend-overlay backdrop-blur-sm ',
-        className,
-      )}
-    >
-      <div className='max-w-global-inner-width mx-auto flex h-header w-full items-center gap-4 border-b-2 border-solid'>
-        <button onClick={window.print}>PDF 저장</button>
-        <Link to='/info'>INFO</Link>
+    <div className={cn('fixed z-10 h-header w-full', 'header-print', className)}>
+      <div className='mx-auto flex h-full w-full max-w-global-inner-width items-center justify-between gap-2 border-b-2 border-solid  bg-white bg-opacity-[0.8] px-4 bg-blend-overlay backdrop-blur-sm md:px-10'>
+        <Logo className='size-20 cursor-pointer md:size-auto' onClick={() => navigator('/info')} />
 
-        {projectKeys.map((key) => (
-          <Link to={`/project/${key}`} key={`${key}-${uniqId}`}>
-            PROJECT : {key.toUpperCase()}
-          </Link>
-        ))}
+        <div className='hidden md:block'>
+          <Link to='/info'>INFO</Link>
+
+          {/* TODO: 드롭다운으로 변경 */}
+          {projectKeys.map((key) => (
+            <Link to={`/project/${key}`} key={`${key}-${uniqId}`}>
+              PROJECT : {key.toUpperCase()}
+            </Link>
+          ))}
+          <button className={buttons.default} onClick={window.print}>
+            PDF 저장
+          </button>
+        </div>
+
+        <button className={cn(buttons.default, 'block md:hidden')}>햄버거</button>
       </div>
     </div>
   );
 };
+
+const Link = (props: ComponentProps<typeof RouterLink>) => (
+  <RouterLink className={buttons.default} {...props} />
+);
