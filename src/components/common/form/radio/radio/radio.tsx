890d49f378372589cdbee8dd05e56525127b5c1a
seo <seo0h9@gmail.com>
2024-03-25 21:10:30 +0900

    [SP-29] ♻️ Fix. 라디오가 체크된 경우의 스타일을 외부에서 주입받도록 수정

--- src/components/common/form/radio/radio/radio.tsx
+++ src/components/common/form/radio/radio/radio.tsx
@@ -11,9 +11,10 @@ import type { UseRadioProps } from '../types';
 type Omitted = 'onChange' | 'defaultChecked' | 'checked' | 'type' | 'value' | 'className';
 
 type RadioStyleProps = {
-  labelClassName?: string;
-  textClassName?: string;
-  inputClassName?: string;
+  labelStyle?: string;
+  textStyle?: string;
+  inputStyle?: string;
+  checkedStyle?: string;
 };
 
 export interface RadioProps
@@ -29,9 +30,10 @@ export const Radio = forwardRef<HTMLInputElement, RadioProps>(function Radio(
     children,
     onChange: onChangeProp,
     value,
-    inputClassName,
-    labelClassName,
-    textClassName,
+    inputStyle,
+    labelStyle,
+    textStyle,
+    checkedStyle,
     ...rest
   } = props;
 
@@ -63,14 +65,10 @@ export const Radio = forwardRef<HTMLInputElement, RadioProps>(function Radio(
   return (
     <label
       htmlFor={radioInputProps.id}
-      className={cn(
-        labelClassName,
-        `custom-radio`,
-        `${radioInputProps.checked ? `bg-slate-100` : `bg-none`}`,
-      )}
+      className={cn(labelStyle, `custom-radio`, `${radioInputProps.checked && checkedStyle}`)}
     >
-      <input type='radio' className={inputClassName} {...radioInputProps} />
-      <span className={textClassName}>{children}</span>
+      <input type='radio' className={inputStyle} {...radioInputProps} />
+      <span className={textStyle}>{children}</span>
     </label>
   );
 });
