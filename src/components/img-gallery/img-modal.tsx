ec5e6b78cc8de205278ba2e73e87c54cdd7db857
seo <seo0h9@gmail.com>
2024-03-26 20:14:28 +0900

    [SP-30] 🐛 Fix. image modal 잘림 현상 수정

--- src/components/img-gallery/img-modal.tsx
+++ src/components/img-gallery/img-modal.tsx
@@ -18,12 +18,16 @@ export const ImageModal = ({
         {...{ onClick }}
       >
         <div className='absolute top-0 z-10 flex h-full w-full items-center justify-center'>
-          <div className='over flex h-fit max-h-[80%] max-w-lg flex-col gap-2 overflow-hidden rounded-xl border border-gray-400 bg-gray-800 bg-opacity-30 p-3 text-white backdrop-blur-sm'>
-            <img className={cn('rounded-md object-cover', className)} {...props} />
-            <h2 className='text-xl font-bold'>{caption?.heading}</h2>
-            <p className='break-words break-keep text-sm font-light [&_a]:underline'>
-              <Markdown>{caption?.descriptions}</Markdown>
-            </p>
+          <div className='flex h-[60vh] w-[50ch] flex-col justify-around rounded-xl border border-gray-400 bg-gray-800 bg-opacity-30 p-3 text-white backdrop-blur-sm'>
+            <div className='max-h-[80%] overflow-hidden rounded-md bg-slate-800'>
+              <img className={cn('h-full w-full object-contain py-2', className)} {...props} />
+            </div>
+            <div>
+              <h2 className='text-xl font-bold'>{caption?.heading}</h2>
+              <p className='break-words break-keep text-sm font-light [&_a]:underline'>
+                <Markdown>{caption?.descriptions}</Markdown>
+              </p>
+            </div>
           </div>
         </div>
         <div className='z-10 h-full bg-slate-800 bg-opacity-60 ' />
