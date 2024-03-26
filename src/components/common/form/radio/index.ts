1e2aaa963dfb28ee6422c8d4c142b39e2ba05afd
seo <seo0h9@gmail.com>
2024-03-26 19:25:39 +0900

    [SP-29] ✏️  Fix. 잘못된 컴포넌트 경로 수정

--- src/components/common/form/radio/index.ts
+++ src/components/common/form/radio/index.ts
@@ -1,6 +1,6 @@
 export { Radio } from './radio/radio';
 export { RadioGroup } from './radio-group/radio-group';
-export { useRadioGroupContext } from './radio-group/radio-group';
+export { useRadioGroupContext } from './radio-group/context';
 
 export { useRadio } from './radio/use-radio';
 export { useRadioGroup } from './radio-group/use-radio-group';
