b57e182b4094f603fb3a458d386005529b3df6b8
seo <seo0h9@gmail.com>
2024-03-26 19:59:01 +0900

    [SP-30] 🩹 Fix. nested p in p

--- src/components/mdx/utils/markdown-parer.tsx
+++ src/components/mdx/utils/markdown-parer.tsx
@@ -3,5 +3,9 @@ import { Options, default as ReactMarkdown } from 'react-markdown';
 import { components } from '@/components/mdx';
 
 export const Markdown = (props: Options) => {
-  return <ReactMarkdown components={{ ...components }}>{props.children}</ReactMarkdown>;
+  return (
+    <ReactMarkdown unwrapDisallowed disallowedElements={['p']} components={{ ...components }}>
+      {props.children}
+    </ReactMarkdown>
+  );
 };
