1e59a0fd2eb0c8665def0200ab59e12c09e1aacd
seo <seo0h9@gmail.com>
2024-03-29 14:10:34 +0900

    [SP-37] 💄 Update. print 시 보여지는 ui 수정

--- src/components/layout/global.tsx
+++ src/components/layout/global.tsx
@@ -1,7 +1,7 @@
 export const GlobalLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='mx-auto max-w-global-inner-width bg-white p-header px-4 md:px-10'>
-      <div className='pt-2'>{children}</div>
+    <div className='mx-auto max-w-global-inner-width bg-white p-header px-4 md:px-10 print:p-0'>
+      <div className='pt-2 print:p-0'>{children}</div>
     </div>
   );
 };
