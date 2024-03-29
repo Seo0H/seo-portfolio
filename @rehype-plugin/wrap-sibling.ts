769774f12ab4079674aa4376f70f627183319f15
seo <seo0h9@gmail.com>
2024-03-29 13:31:06 +0900

    [SP-37] ✨ Add. 형제를 선택해서 래핑하는 rehype 플러그인 구현

---
+++ @rehype-plugin/wrap-sibling.ts
@@ -0,0 +1,72 @@
+import { parseSelector } from 'hast-util-parse-selector';
+import { selectAll } from 'hast-util-select';
+import { visit } from 'unist-util-visit';
+
+import type { ElementContent, Nodes } from 'hast';
+
+interface Option {
+  /**
+   * support selector syntax
+   * url : https://github.com/syntax-tree/hast-util-select?tab=readme-ov-file#support
+   * {@link selectAll}
+   */
+  selector: string;
+  wrapper?: string;
+}
+
+function transform(tree: Nodes, { selector, wrapper }: Option) {
+  if (typeof wrapper !== 'string') {
+    throw new TypeError('Expected a `string` as wrapper');
+  }
+
+  if (typeof selector !== 'string') {
+    throw new TypeError('Expected a `string` as selector');
+  }
+
+  for (const match of selectAll(selector, tree))
+    visit(tree, match, (node, idx, parent) => {
+      if (idx === undefined || parent === undefined) return;
+
+      const wrap = parseSelector(wrapper);
+      let upperSibling: ElementContent | undefined = undefined;
+      let upperSiblingIdx = -1;
+
+      for (let i = idx; i >= 0; i--) {
+        const target = parent.children[i];
+        if ('tagName' in target === false) continue;
+        const isHeadingType = target.tagName[0] === 'h';
+
+        if (isHeadingType) {
+          upperSibling = target as ElementContent;
+          upperSiblingIdx = i;
+          break;
+        }
+      }
+
+      if (!upperSibling) return;
+
+      wrap.children = [upperSibling, node];
+
+      parent.children = [
+        ...parent.children.slice(0, upperSiblingIdx),
+        wrap,
+        ...parent.children.slice(idx + 1),
+      ];
+    });
+}
+
+export default (allOptions: Option[]) => {
+  return (tree: Nodes) => {
+    if (allOptions == null) {
+      throw new TypeError('Expected a `string` or an `array` as options');
+    }
+
+    if (Array.isArray(allOptions)) {
+      allOptions.forEach((options) => {
+        transform(tree, options);
+      });
+    } else {
+      transform(tree, allOptions);
+    }
+  };
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
 import mdx from '@mdx-js/rollup';
-import remarkFrontmatter from 'remark-frontmatter';
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
-import remarkGfm from 'remark-gfm';
+import react from '@vitejs/plugin-react';
 import rehypePrettyCode, { type Options } from 'rehype-pretty-code';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkGfm from 'remark-gfm';
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
+import { defineConfig } from 'vite';
+
+import rehypeWrapSibling from './@rehype-plugin/wrap-sibling';
 
 const prettyCodeOptions: Options = {
   theme: 'nord',
@@ -22,7 +24,13 @@ export default defineConfig({
       ...mdx({
         providerImportSource: '@mdx-js/react',
         remarkPlugins: [remarkFrontmatter, [remarkMdxFrontmatter, { name: 'matter' }], remarkGfm],
-        rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+        rehypePlugins: [
+          [rehypePrettyCode, prettyCodeOptions],
+          [
+            rehypeWrapSibling,
+            { selector: 'h2 + *, h3 + *, h4 + *', wrapper: 'div.print-breakpoint' },
+          ],
+        ],
       }),
     },
     react(),
