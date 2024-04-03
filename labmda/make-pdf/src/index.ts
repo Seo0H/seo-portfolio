4ae5c291fae1a32291d8a65aacbf9287a48b7b20
seo <seo0h9@gmail.com>
2024-04-03 14:57:04 +0900

    [SP-37] ✨ Add. PDF 생성 및 S3 저장 관련 lambda 코드 추가

---
+++ labmda/make-pdf/src/index.ts
@@ -0,0 +1,128 @@
+("use strict");
+var exports = {};
+
+import chromium from "@sparticuz/chromium";
+import AWS, { type S3 as TS3 } from "aws-sdk";
+import PDFMerger from "pdf-merger-js";
+import puppeteer, { type Browser } from "puppeteer-core";
+
+import type { Handler } from "aws-lambda";
+
+const merger = new PDFMerger();
+// TODO: 마이그래이션 @aws-sdk/client-s3
+const S3 = new AWS.S3({
+  signatureVersion: "v4",
+});
+
+const bucketName = process.env.BUCKET_NAME ?? "";
+
+const handler: Handler = async () => {
+  console.log(`✅ Start make PDF lambda function`);
+
+  let browser: Browser | null = null;
+  let page: puppeteer.Page | null = null;
+  let result: ReturnType<typeof S3.putObject> | null = null;
+
+  try {
+    // launch the browser
+    browser = await puppeteer.launch({
+      args: [
+        "--disable-features=site-per-process",
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--single-process",
+      ],
+      defaultViewport: chromium.defaultViewport,
+      executablePath: process.env.AWS_EXECUTION_ENV
+        ? await chromium.executablePath(
+            "/var/task/node_modules/@sparticuz/chromium/bin"
+          )
+        : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+      headless: !!chromium.headless,
+    });
+
+    page = await browser.newPage();
+
+    console.log(`✅ Make page success`);
+
+    // TODO: url hard coding 제거
+    await page.goto("https://portfolio.seo0h.me/info", {
+      waitUntil: "networkidle2",
+    });
+
+    await merger.add(
+      await page.pdf({
+        format: "a4",
+        printBackground: true,
+      })
+    );
+
+    await page.goto("https://portfolio.seo0h.me/project/blog", {
+      waitUntil: "networkidle2",
+    });
+
+    await merger.add(
+      await page.pdf({
+        format: "a4",
+        printBackground: true,
+      })
+    );
+
+    await page.goto("https://portfolio.seo0h.me/project/sobisa", {
+      waitUntil: "networkidle2",
+    });
+
+    await merger.add(
+      await page.pdf({
+        format: "a4",
+        printBackground: true,
+      })
+    );
+
+    await merger.setMetadata({
+      producer: "react, AWS Lambda, serverless, puppeteer ",
+      author: "seo0h",
+      creator: "seo0h",
+      title: "seo0h portfolio",
+    });
+
+    // upload the screenshot in s3
+    const bucketParams: TS3.Types.PutObjectRequest = {
+      Body: await merger.saveAsBuffer(),
+      Bucket: bucketName,
+      ContentType: "application/pdf",
+      CacheControl: "max-age=31536000",
+      Key: "seo-portfolio.pdf",
+      StorageClass: "STANDARD",
+    };
+
+    console.log(`✅ All pdf merge success`);
+
+    result = await S3.putObject(bucketParams)
+      .promise()
+      .then((data) => {
+        console.log("✅ Successfully PUT object");
+        console.log(data);
+        return {
+          screenshotUrl: `https://${bucketName}.s3.amazonaws.com/${bucketParams.Key}`,
+          ...data,
+        };
+      })
+      .catch((error) => {
+        console.log("⚠️ Failed to PUT object");
+        console.log(error);
+        return error;
+      });
+  } catch (error) {
+    console.log(`⚠️ something wrong...`);
+    console.log(error);
+    return error;
+  } finally {
+    if (browser !== null) await browser.close();
+    if (page !== null) await page;
+  }
+  return { message: "Finish Lambda Function." };
+};
+
+export { handler };
