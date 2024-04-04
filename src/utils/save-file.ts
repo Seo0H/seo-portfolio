6274a0122827177749aae1f4d3e2382c8bbdf861
seo <seo0h9@gmail.com>
2024-04-04 15:28:27 +0900

    [SP-37] ♻️ Fix. saveFile util func 의 재사용성 및 가독성 향상

--- src/utils/save-file.ts
+++ src/utils/save-file.ts
@@ -1,15 +1,20 @@
 /**
  *
  * @param blob File data
- * @param name 확장자와 같이 작성 필요 (ex - 무제.pdf)
+ * @param name 확장자와 같이 작성 필요 (ex - Untitled.pdf)
+ * @param ms createObjectUrl 메무리 유지 시간
  */
-export const saveFile = async (blob: Blob, name: string = '무제.pdf') => {
+export const saveFile = async (
+  blob: Blob,
+  name: string = 'Untitled.pdf',
+  ms: number = 30 * 1000,
+) => {
   const a = document.createElement('a');
   a.download = name;
   a.href = URL.createObjectURL(blob);
 
   a.addEventListener('click', () => {
-    setTimeout(() => URL.revokeObjectURL(a.href), 30 * 1000);
+    setTimeout(() => URL.revokeObjectURL(a.href), ms);
   });
   a.click();
 };
