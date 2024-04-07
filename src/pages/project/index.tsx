671d283ac1a33f4bad8ed4e44aaf5a046771e525
seo <seo0h9@gmail.com>
2024-04-07 20:25:16 +0900

    💄 Fix. Print 시 보여지는 프로젝트 UI 수정

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -17,35 +17,36 @@ const ProjectPage = () => {
   const projectSubTitle = `Project ${projectKeys.indexOf(id) + 1}`;
 
   return (
-    <div className='flex flex-col gap-1 print:gap-1 [&_.print-breakpoint]:print:break-inside-avoid'>
+    <div className='flex flex-col gap-1'>
       {/* PC View */}
-      <RoundedBlock
-        className={`hidden justify-between bg-peacock-800 pb-[20px] text-[12px] font-bold text-peacock-200
-                    md:flex print:rounded-none print:bg-white print:py-1 print:font-medium print:text-gray-500`}
-      >
-        <span>{projectSubTitle}</span>
-        <span>{matter.duration}</span>
-      </RoundedBlock>
+      <div className='print:hidden'>
+        <RoundedBlock
+          className={`hidden justify-between bg-peacock-800 pb-[20px] text-[12px] font-bold text-peacock-200 md:flex`}
+        >
+          <span>{projectSubTitle}</span>
+          <span>{matter.duration}</span>
+        </RoundedBlock>
 
-      <RoundedBlock
-        className={`hidden translate-y-[-20px] items-center justify-between border border-gray-200 md:flex  print:translate-y-0 `}
-      >
-        <div className='flex flex-col gap-1'>
-          <h1 className='text-sm font-bold leading-tight text-gray-500 print:font-medium'>
-            {matter.subTitle}
-          </h1>
-          <h1 className='text-3xl font-bold'>{matter.title}</h1>
-        </div>
+        <RoundedBlock
+          className={`hidden translate-y-[-20px] items-center justify-between border border-gray-200 md:flex  print:translate-y-0 `}
+        >
+          <div className='flex flex-col gap-1'>
+            <h1 className='text-sm font-bold leading-tight text-gray-500 print:font-medium'>
+              {matter.subTitle}
+            </h1>
+            <h1 className='text-3xl font-bold'>{matter.title}</h1>
+          </div>
 
-        <div className='hidden max-w-[60%] flex-wrap items-end justify-end gap-1 md:flex'>
-          {matter.skillTag.map((skill) => (
-            <SkillTag key={`${uniqId}-${skill}`} skill={skill} />
-          ))}
-        </div>
-      </RoundedBlock>
+          <div className='hidden max-w-[60%] flex-wrap items-end justify-end gap-1 md:flex'>
+            {matter.skillTag.map((skill) => (
+              <SkillTag key={`${uniqId}-${skill}`} skill={skill} />
+            ))}
+          </div>
+        </RoundedBlock>
+      </div>
 
       {/* Mobile View */}
-      <div className='flex flex-col gap-2 md:hidden'>
+      <div className='flex flex-col gap-2 md:hidden print:block'>
         <h1 className='text-base font-semibold text-peacock-800'>{projectSubTitle}</h1>
         <span className='text-3xl font-extrabold text-black'>{matter.title}</span>
 
