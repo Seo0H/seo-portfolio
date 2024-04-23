a88452de46870af1a703e627b3595c59c65913ac
seo <seo0h9@gmail.com>
2024-04-23 22:41:36 +0900

    📝 Docs. 포트폴리오 컨텐츠 추가

--- contents/project/blog/contents.ts
+++ contents/project/blog/contents.ts
@@ -1,8 +1,18 @@
 import type { MainImageItemContents } from '@contents/components/main-image/types';
 
 export const contents: MainImageItemContents[] = [
-  { src: '/posts/blog/1.png', className: 'row-span-2 col-span-3 ', isPrint: true },
-  { src: '/posts/blog/4.png', className: '', isPrint: true },
-  { src: '/posts/blog/2.png', className: '', isPrint: true },
-  { src: '/posts/blog/3.png', isPrint: true },
+  {
+    src: '/posts/blog/1.png',
+    className: 'row-span-2 col-span-3 ',
+    isPrint: true,
+    caption: { heading: 'Web main page' },
+  },
+  { src: '/posts/blog/4.png', className: '', isPrint: true, caption: { heading: 'Web post page' } },
+  {
+    src: '/posts/blog/2.png',
+    className: '',
+    isPrint: true,
+    caption: { heading: 'Mobile main page' },
+  },
+  { src: '/posts/blog/3.png', isPrint: true, caption: { heading: 'Mobile post page' } },
 ];
