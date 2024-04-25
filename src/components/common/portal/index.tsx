9cb00b8c9ce8088c3f88eb9d120207d593bb7a29
seo <seo0h9@gmail.com>
2024-04-25 22:29:11 +0900

    🔥 Remove. 중복 코드 제거

--- src/components/common/portal/index.tsx
+++ src/components/common/portal/index.tsx
@@ -2,6 +2,6 @@ import { ComponentProps } from 'react';
 
 import { createPortal } from 'react-dom';
 
-export const Portal = ({ children }: { children: React.ReactNode } & ComponentProps<'div'>) => {
+export const Portal = ({ children }: ComponentProps<'div'>) => {
   return createPortal(children, document.body);
 };
