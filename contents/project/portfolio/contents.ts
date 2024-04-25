a6209744e6949be9143a9f587e80a976f9b5b2ac
seo <seo0h9@gmail.com>
2024-04-26 02:10:08 +0900

    📝 Docs. project portfolio 초안 작성

---
+++ contents/project/portfolio/contents.ts
@@ -0,0 +1,28 @@
+import { MainImageItemContents } from '@contents/components/main-image/types';
+
+import { SkillTag } from '@/types/front-matter';
+
+export const skillTags: SkillTag[] = [
+  'TypeScript',
+  'Vite',
+  'MDX',
+  'Tailwind',
+  'Github Page',
+  'SupaBase',
+  'S3',
+  'Lambda',
+];
+
+export const contents: MainImageItemContents[] = [
+  {
+    src: '/posts/portfolio/1.png',
+    className: 'row-span-3 col-span-2',
+    caption: { heading: 'Project page PC ver' },
+  },
+  {
+    src: '/posts/portfolio/2.png',
+    className: 'row-span-2',
+    caption: { heading: 'Project page mobile ver' },
+  },
+  { src: '/posts/portfolio/3.png', caption: { heading: 'Image Modal' } },
+];
