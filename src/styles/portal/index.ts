fb5a56a45af90c59db1eb326054ccb3658aace92
seo <seo0h9@gmail.com>
2024-04-30 11:27:46 +0900

    ♻️ Refactor. 포탈 활성화 시 보여지는 배경을 컴포넌트로 분리

---
+++ src/styles/portal/index.ts
@@ -0,0 +1,3 @@
+export const portal = /*tw*/ {
+  background: `bg-slate-800 bg-opacity-60 fixed left-0 top-0 z-20 h-full max-h-full w-full overflow-hidden backdrop-blur-sm dark:bg-slate-950 dark:bg-opacity-60`,
+};
