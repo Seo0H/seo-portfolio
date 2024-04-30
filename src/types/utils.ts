eac55e02469b46827f06cd70116deb732a685c0e
seo <seo0h9@gmail.com>
2024-04-30 18:09:11 +0900

    👔 Add. project key, title, front matter 를 분리한 projectInfo 객체 구현

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,3 +1,6 @@
 export const getKeyFromObject = <KeyType>(target: object) => {
   return Object.keys(target) as KeyType[];
 };
+
+export const getEntryFromObject = <KeyType extends string, ValueType>(target: object) =>
+  Object.entries(target) as [KeyType, ValueType][];
