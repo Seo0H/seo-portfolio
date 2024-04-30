6ddd06c7b16facf761fff4ff0078a0b9fd7a2f33
seo <seo0h9@gmail.com>
2024-04-30 18:04:49 +0900

    🍱 Add. icon 추가

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -3,3 +3,4 @@ export { Ellipsis } from './ellipsis';
 export { Spinner } from './spinner';
 export { Arrow } from './arrow';
 export { X } from './x-mark';
+export * from './ect-icon';
--- src/components/button/dark-mode/index.tsx
+++ src/components/button/dark-mode/index.tsx
@@ -1,9 +1,13 @@
-import { ComponentProps, useLayoutEffect } from 'react';
+import { ComponentProps, useLayoutEffect, useState } from 'react';
 
-import { storageData } from '@/constant/stoarge';
+import { Icon } from '@/asset';
+import { type ThemeValues, storageData } from '@/constant/stoarge';
 
 export const ToggleDarkMode = (props: Omit<ComponentProps<'button'>, 'onClick'>) => {
   const { THEME } = storageData;
+  const [curTheme, setCurTheme] = useState<ThemeValues | null>(
+    localStorage.getItem(THEME.KEY) as ThemeValues | null,
+  );
 
   useLayoutEffect(() => {
     const isStorageThemeDark = localStorage.theme === THEME.DARK;
@@ -20,12 +24,18 @@ export const ToggleDarkMode = (props: Omit<ComponentProps<'button'>, 'onClick'>)
   const handleToggleDarkMode = () => {
     if (document.body.classList.contains(THEME.DARK)) {
       localStorage.setItem(THEME.KEY, THEME.LIGHT);
+      setCurTheme(THEME.LIGHT);
     } else {
       localStorage.setItem(THEME.KEY, THEME.DARK);
+      setCurTheme(THEME.DARK);
     }
 
     document.body.classList.toggle(THEME.DARK);
   };
 
-  return <button {...props} onClick={handleToggleDarkMode} />;
+  return (
+    <button {...props} onClick={handleToggleDarkMode}>
+      {curTheme === 'light' ? <Icon.Sun /> : <Icon.Moon />}
+    </button>
+  );
 };
