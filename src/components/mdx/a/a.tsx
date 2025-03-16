22ec36884f414be38b7ee5cb8c7b26b20dfbe264
seo <seo0h9@gmail.com>
2025-03-16 18:15:47 +0900

    fix. 배포 링크 및 메타 태그 URL 업데이트

--- src/components/mdx/a/a.tsx
+++ src/components/mdx/a/a.tsx
@@ -1,18 +1,18 @@
-import { ComponentProps, useState } from 'react';
+import { ComponentProps } from 'react';
 
 import ReactGA from 'react-ga4';
 
 // import { OGPreviewCard } from '@/components/og-preview';
-import { debounce } from '@/utils/debounce';
+// import { debounce } from '@/utils/debounce';
 
 export const A = (
   props: Omit<ComponentProps<'a'>, 'onMouseEnter' | 'onMouseLeave' | 'onClick'>,
 ) => {
-  const [isHover, setIsHover] = useState(false);
-  const isPreview = props.title === 'preview';
+  // const [isHover, setIsHover] = useState(false);
+  // const isPreview = props.title === 'preview';
 
-  const handleMouseEnter = debounce(() => setIsHover(true), 100);
-  const handleMouseLeave = debounce(() => setIsHover(false), 100);
+  // const handleMouseEnter = debounce(() => setIsHover(true), 100);
+  // const handleMouseLeave = debounce(() => setIsHover(false), 100);
   const handleClick = () => {
     ReactGA.event({
       category: 'MDX Link',
@@ -27,8 +27,8 @@ export const A = (
       {/* {isHover && <OGPreviewCard url={props.href ?? ''} />} */}
       <a
         target='_blank'
-        onMouseEnter={isPreview ? handleMouseEnter : undefined}
-        onMouseLeave={isPreview ? handleMouseLeave : undefined}
+        // onMouseEnter={isPreview ? handleMouseEnter : undefined}
+        // onMouseLeave={isPreview ? handleMouseLeave : undefined}
         onClick={handleClick}
         {...props}
       />
