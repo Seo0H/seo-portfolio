834324faf2e28a52819de4b326c33f1a476507bc
seo <seo0h9@gmail.com>
2025-03-16 17:27:50 +0900

    docs. 포트폴리오 인덱스, 스킬셋 업데이트

---
+++ contents/project/form/contents.ts
@@ -0,0 +1,7 @@
+import { MainImageItemContents } from '@contents/components/main-image/types';
+
+import { SkillTag } from '@/types/front-matter';
+
+export const skillTags: SkillTag[] = [`TypeScript`, `Next.js`, `Zod`, `Jest`, `Husky`, `Tailwind`];
+
+export const contents: MainImageItemContents[] = [];
